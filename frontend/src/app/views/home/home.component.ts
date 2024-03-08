93565d8491cebb399a1167726278712842a9dba6
manuel <manuelfqc@gmail.com>
2024-03-08 04:08:13 -0500

    [08/03/2024 04:08] Update

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -11,6 +11,10 @@ export class HomeComponent {
 
   userArray: UserModel[] = [];
   selectedUser: UserModel = new UserModel(0, '', '');
+  isSortedById: boolean = false;
+  isSortedByName: Boolean = false;
+  isSortedByDate: Boolean = false;
+  searchText: string = '';
 
   constructor(private userService: UserService) {
     this.updateTable();
@@ -45,6 +49,52 @@ export class HomeComponent {
         this.userService.setSelectedUser(this.selectedUser);
       },
     });
+  }
+
+  sortByIdUp(a: UserModel, b: UserModel) {
+    return a.identification - b.identification;
+  }
+
+  sortByIdDown(a: UserModel, b: UserModel) {
+    return b.identification - a.identification;
+  }
+
+  sortById() {
+    this.isSortedById = !this.isSortedById;
+    if (this.isSortedById) {
+      this.userArray.sort(this.sortByIdUp);
+    } else {
+      this.userArray.sort(this.sortByIdDown);
+    }
+  }
+
+  filter() {
+    if (this.searchText != '') {
+      this.userArray = this.userArray.filter((e) => {
+        return e.name.includes(this.searchText) ||
+          e.birth_date.includes(this.searchText) ||
+          e.identification.toString().includes(this.searchText);
+      });
+    } else {
+      this.updateTable();
+    }
+  }
+
+  sortByName(a: UserModel, b: UserModel) {
+    const nameA = a.name.toLowerCase();
+    const nameB = b.name.toLowerCase();
+
+    if (nameA < nameB) {
+      return -1;
+    } else if (nameA > nameB) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  sortByNames(){
+    this.userArray.sort(this.sortByName);
   }
 
 }
